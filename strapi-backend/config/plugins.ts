06b33c3f445e4475c2bf7020a71ce506d3c7a096
mam <maksimmuhametzyanov11@gmail.com>
2026-02-01 00:53:00 +0100

    strapi set

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => {
+  // Generate JWT secret if not provided (for easy deployment)
+  const generateJWTSecret = () => {
+    const crypto = require('crypto');
+    return crypto.randomBytes(16).toString('base64');
+  };
+
+  return {
+    'users-permissions': {
+      jwtSecret: env('JWT_SECRET', generateJWTSecret()),
+    },
+  };
+};
